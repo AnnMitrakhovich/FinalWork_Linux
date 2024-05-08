34150f083498c793aa78413c8f2d3b7b607dbeeb
Ann <ann.mitrakhovich@gmail.com>
2024-05-08 13:01:09 +0300

    add class Animal and check connection with git

---
+++ src/Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+    private String name;
+    private String command;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+}
