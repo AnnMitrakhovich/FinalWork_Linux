b8d60d1ffa6f590a25721482927548babb88048e
Ann <ann.mitrakhovich@gmail.com>
2024-05-08 12:48:41 +0300

    add animal

---
+++ src/Animal.java
@@ -0,0 +1,24 @@
+public class Animal {
+    private String name;
+    private String command;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+}
